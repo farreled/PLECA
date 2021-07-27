eca292aaeeefa5197431d00befa7d1834ad87186
Ned Farrell <nedf1999@live.com>
2021-07-27 14:52:15 -0400

    added BPrime and test cases in main

---
+++ PLEC/BPrime.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace PLEC
+{
+    class BPrime
+    {
+        public BigInteger primeNum;
+
+        public BPrime()
+        {
+            primeNum = new BigInteger(2);
+        }
+
+        public BPrime(BPrime b)
+        {
+            primeNum = b.primeNum;
+        }
+
+        public BPrime(String s)
+        {
+            try
+            {
+                BigInteger temp = new BigInteger();
+                temp = BigInteger.Parse(s);
+
+                if (!isPrime(temp))
+                {
+                    throw new Exception(s + " is not a prime number");
+                }
+                else
+                {
+                    primeNum = temp;
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An integer must be entered!");
+                return;
+            }
+        }
+
+        public bool isPrime(BigInteger prime)
+        {
+            if (prime <= 1)
+            {
+                return false;
+            }
+            if (prime == 2)
+            {
+                return true;
+            }
+            if (prime % 2 == 0)
+            {
+                return false;
+            }
+
+            int root = (int)Math.Floor(Math.Sqrt((double)prime));
+
+            for (int i = 3; i <= root; i += 2)
+            {
+                if (prime % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public BPrime Increment()
+        {
+            BigInteger nextPrime = primeNum + 1;
+
+            while (!isPrime(nextPrime))
+            {
+                nextPrime++;
+            }
+
+            primeNum = nextPrime;
+            return this;
+        }
+
+        public BPrime Decrement()
+        {
+            if (primeNum == 2)
+            {
+                throw new Exception("Prime number cannot be decremented any more");
+            }
+
+            BigInteger nextPrime = primeNum - 1;
+
+            while (!isPrime(nextPrime))
+            {
+                nextPrime--;
+            }
+
+            primeNum = nextPrime;
+            return this;
+        }
+
+        public BPrime Add(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Increment();
+            }
+
+            return this;
+        }
+
+        public BPrime Assign(BPrime b)
+        {
+            primeNum = b.primeNum;
+            return this;
+        }
+
+        public String toString()
+        {
+            return primeNum.ToString();
+        }
+    }
+}
--- PLEC/Program.cs
+++ PLEC/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace PLEC
 {
@@ -10,6 +11,51 @@ namespace PLEC
     {
         static void Main(string[] args)
         {
+            //Basic demonstration
+            //Constructor
+            BPrime bPrime = new BPrime();
+            Console.WriteLine("bPrime's value is: {0}", bPrime.primeNum);
+
+            bPrime.primeNum = 11;
+
+            //Copy constructor
+            BPrime bPrime2 = new BPrime(bPrime);
+            Console.WriteLine("bPrime's value is: {0} and bPrime2's value is {1}", bPrime.primeNum, bPrime2.primeNum);
+
+            //Initialize constructor
+            BPrime bPrime3 = new BPrime("17");
+            Console.WriteLine("bPrime3's value is: {0}", bPrime3.primeNum);
+
+            //Increment
+            Console.WriteLine("\nbPrime's value before incrementing is: {0}", bPrime.primeNum);
+            bPrime.Increment();
+            Console.WriteLine("bPrime's value after incrementing is: {0}", bPrime.primeNum);
+
+            //Decrement
+            Console.WriteLine("\nbPrime2's value before decrementing is: {0}", bPrime2.primeNum);
+            bPrime2.Decrement();
+            Console.WriteLine("bPrime2's value after decrementing is: {0}", bPrime2.primeNum);
+
+            //Add
+            Console.WriteLine("\nbPrime3's value before adding 5 is: {0}", bPrime3.primeNum);
+            bPrime3.Add(5);
+            Console.WriteLine("bPrime3's value after adding 5 is: {0}", bPrime3.primeNum);
+
+            //Assign
+            BigInteger prev = bPrime.primeNum;
+            bPrime.Assign(bPrime3);
+            Console.WriteLine("\nbPrime used to be {0} but is now {1}", prev, bPrime.primeNum);
+
+            //ToString
+            string primeStr = bPrime.toString();
+            Console.WriteLine("\nProving this is a string by padding left");
+            Console.WriteLine(primeStr.PadLeft(5));
+
+            //Demonstrating that BPrime can handle 50 digits
+            string strBigPrime = "29907534584936057305785597301561692929713247109403";
+            Console.WriteLine("\nbigPrime is {0} digits long", strBigPrime.Length);
+            BPrime bigPrime = new BPrime(strBigPrime);
+            Console.WriteLine("bigPrime's value is: {0}", bigPrime.primeNum);
         }
     }
 }
